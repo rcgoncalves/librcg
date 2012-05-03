adf2b0f7db9d9bb666a24745dd309033dd95a2bf
rcg <rcgoncalves.pt@gmail.com>
2012-05-02 23:54:25 -0500

    Started revision of "rlp".

--- rlp.h
+++ rlp.h
@@ -1,40 +1,50 @@
 /**
- * Implementação de funções de programação linear.
+ * Implementation of linear programming functions.
  *
- * @author Rui Carlos A. Gonçalves <rcgoncalves.pt@gmail.com>
+ * @author Rui Carlos Gonçalves <rcgoncalves.pt@gmail.com>
  * @file rlp.h
- * @version 2.1.1
- * @date 02/2009
+ * @version 3.0
+ * @date 05/2012
  */
-#ifndef _RLP_
-#define _RLP_
+#ifndef _RLP_H_
+#define _RLP_H_
 
 /**
- * Aplica o @a Algoritmo @a Simplex a um problema de optimização (programação
- *   linear).
- * Dado um problema de @a n variáveis (@a x1 , ... , @a xn ) e @a m condições
- *   (@a c1=b1 , ... , @a cm=bm ), a função deve receber uma matriz @a a de
- *   dimensão (m+1)*(n+m+2) contendo:
- * @li em a[0][i-1] (para @a i de @a 1 até @a n ) o coeficiente da variável @a
- *     xi na expressão a minimizar;
- * @li em a[0][i] (para @a i de @a n até @a n+m+1 ) o valor 0;
- * @li em a[i][j-1] (para @a i de @a 1 até @a m e @a j de @a 1 até @a n ) o
- *     coeficiente da variável @a xj na condição @a ci;
- * @li em a[i][j] (para @a i de @a 1 até @a m e @a j de @a n até @a n+m-1 ) a
- *     matriz identidade;
- * @li em a[i][n+m] (para @a i de 1 até @a m ) o valor @a bi;
- * @li em a[i][n+m+1] (para @a i de 1 até @a m ) o valor @a n+i.
+ * Applies the <em>Simplex Algorithm</em> to an optimization problem.
  *
- * Permite definir um ficheiro onde são colocadas as várias tabelas resultantes
- *   da aplicação do algoritmo (através da variável @a file).
+ * Given a problem with <em>n</em> variables (<em>x<sub>1</sub></em>, ...,
+ * <em>x<sub>n</sub></em>) and <em>m</em> conditions (<em>c<sub>1</sub> =
+ * b<sub>1</sub></em>, ..., <em>c<sub>m</sub> = b<sub>m</sub></em>), the
+ * function must receive a matrix <tt>a</tt> of size <em>(m+1)*(n+m+2)</em>,
+ * containing:
+ * <ul>
+ *   <li>at <tt>a[0][i-1]</tt> (for <em>i</em> in <em>[1, n]</em>) the
+ *   coefficient of variable <em>x<sub>i</sub></em> in the expression to
+ *   minimize;</li>
+ *   <li>at <tt>a[0][i]</tt> (for <em>i</em> in <em>[n, n+m+1]</em>) the value
+ *   0;</li>
+ *   <li>at <tt>a[i][j-1]</tt> (for <em>i</em> in <em>[1, m]</em>, and for
+ *   <em>j</em> in <em>[1, n]</em>) the coefficient of variable
+ *   <em>x<sub>j</sub></em> in condition <em>c<sub>i</sub></em>;</li>
+ *   <li>at <tt>a[i][j]</tt> (for <em>i</em> in <em>[1, m]</em>, and for
+ *   <em>j</em> in <em>[n, n+m-1]</em>) the identity matrix;</li>
+ *   <li>at <tt>a[i][n+m]</tt> (for <em>i</em> in <em>[1, m]</em>) the value of
+ *   <em>b<sub>i</sub></em>;</li>
+ *   <li>at <tt>a[i][n+m+1]</tt> (for <em>i</em> in <em>[1, m]</em>) the value
+ *   of <em>n+i</em>.</li>
+ * </ul>
  *
- * @param a    matriz que representa o problema (conforme a descrição acima).
- * @param n    número de variáveis da função objectivo.
- * @param m    número de condições.
- * @param file ficheiro onde as tabelas serão impressas (ou NULL).
+ * Allows to specify the file where the table resulting from the application of
+ * the algorithm (using the parameter <em>file</em>).
  *
- * @return 0 se for possível determinar um resultado.
- *         1 caso contrário.
+ * @param a    matrix that represents the problem
+ * @param n    number of variable of the function
+ * @param m    number restrictions
+ * @param file file where the tables will be saved (or <tt>NULL</tt>)
+ *
+ * @return
+ * 0 if it is possible to solve the problem\n
+ * 1 otherwise
  */
 int simplex(double* a,int n,int m,FILE* file);
 
